69ddbcb9b12a62bfd413c87c0304fd40e8a647b5
YourSohit <111377504+YourSohit@users.noreply.github.com>
2025-01-13 08:46:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+API_ID = 17760082
+API_HASH = "c3fc3cd44886967cf3c0e8585b5cad1c"
+BOT_TOKEN = "7728132142:AAGk_57f_5laLd0CuQbcsrdqavdtsWBd0cU"
+UPDATES_CHANNEL = "tcp_bots"
+UPDATES_CHANNEL_ID = -1002015853674
+LOG_CHANNEL_ID = -1001994347895
+FREE_LIMIT = 100
+OWNER_ID = 6116993643
+DATABASE_URL = "mongodb+srv://Shivayfile:jub9vtXuBrJYkw8e@databasefiletolink.k2rei.mongodb.net/?retryWrites=true&w=majority"
+DEVELOPER = "Mr_provider"
+PLAN_PRICE = 99
+PLAN_DURATION = 30  # Days
+TASK_LIMIT = 10  # Maximum tasks per user at a time
